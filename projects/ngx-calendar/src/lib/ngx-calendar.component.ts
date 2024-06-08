195b19ffb727809832c4f30b19907f00e3dfc376
Ernesto Pedrosa Alonso <ernestop891123@gmail.com>
2024-06-08 14:50:00 +0200

    feat: create lib ngx-calendar

---
+++ projects/ngx-calendar/src/lib/ngx-calendar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-calendar',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ngx-calendar works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgxCalendarComponent {
+
+}
