f185da07fc337690080a0f319a9a08bd980a81f8
Ernesto Pedrosa Alonso <pedrosa.e@protonmail.com>
2024-08-08 13:27:31 +0200

    feat: introducing nx to the repo

---
+++ apps/ng-workspace/src/app/services/breadcrumbs.service.ts
@@ -0,0 +1,26 @@
+import { Injectable, inject, signal } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { NavigationEnd, Router } from '@angular/router';
+import { tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BreadcrumbsService {
+  public currentUrlPath = signal<string[]>([]);
+  private _router = inject(Router);
+
+  constructor() {
+    this._router.events
+      .pipe(
+        takeUntilDestroyed(),
+        tap((event) => {
+          if (event instanceof NavigationEnd) {
+            const segments = event.url.split('/').filter((item) => item !== '');
+            this.currentUrlPath.set(segments);
+          }
+        })
+      )
+      .subscribe();
+  }
+}
